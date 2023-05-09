eda8082ecd4cba1f64c3431bbb3a8b713b4a8204
Erik Mägi <erik.maegi@gmail.com>
2023-05-09 23:09:38 +0300

    added docstrings to utils.py

--- gorbslam/common/utils.py
+++ gorbslam/common/utils.py
@@ -11,6 +11,10 @@ from evo.core import metrics, sync, trajectory
 
 
 class CustomJSONEncoder(json.JSONEncoder):
+    """
+    Custom JSON encoder that can handle numpy arrays and dataclasses.
+    """
+
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
@@ -19,21 +23,14 @@ class CustomJSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-def reshape_input(points: np.ndarray, n_inputs: int) -> np.ndarray:
-    mod = points.size % n_inputs
-
-    if mod != 0:
-        rng = np.random.default_rng()
-        n_rows = points.shape[0]
-        rows_to_remove = mod // 3
-        return np.delete(
-            points, rng.choice(n_rows, rows_to_remove, replace=False), axis=0
-        ).reshape(-1, n_inputs)
-
-    return np.array(points).reshape(-1, n_inputs)
-
-
 def downsample(data: np.ndarray, n: int, start=0) -> np.ndarray:
+    """
+    Uniformly downsamples an array of lenght m to an array of length n.
+
+    :param data: The array to downsample.
+    :param n: The length of the downsampled array.
+    :param start: The index of the first element to include in the downsampled array.
+    """
     downsampled_indices = np.linspace(start, data.shape[0] - 1, n, dtype=int)
     downsampled_data = data[downsampled_indices]
     return downsampled_data
@@ -44,6 +41,10 @@ def create_training_splits(
     extra_data: tuple[np.ndarray, np.ndarray],
     validation_split: float,
 ):
+    """
+    Creates training, validation and test splits from the given data.
+    It treats training_data as the validation_split of the total data and will create the validation and test splits from extra_data.
+    """
     n_training = training_data[0].shape[0]
     n_total = math.floor(n_training / (1 - validation_split))
     n_not_training = n_total - n_training
@@ -75,14 +76,10 @@ def assert_trajectory_shape(trajectory: np.ndarray):
     assert trajectory.shape[1] == 3
 
 
-def replace_tum_xyz(tum_data: np.ndarray, xyz: np.ndarray) -> np.ndarray:
-    assert_trajectory_shape(xyz)
-    tum_data_copy = tum_data.copy()
-    tum_data_copy[:, 1:4] = xyz
-    return tum_data_copy
-
-
 def read_tum(file_path: str) -> pd.DataFrame:
+    """
+    Reads a TUM file and returns a pandas DataFrame with the data.
+    """
     return pd.read_csv(
         file_path,
         names=["timestamp", "x", "y", "z", "qx", "qy", "qz", "qw"],
@@ -91,6 +88,10 @@ def read_tum(file_path: str) -> pd.DataFrame:
 
 
 def read_tum_gt(file_path: str) -> pd.DataFrame:
+    """
+    Reads a TUM-like ground truth file where x, y, z are replaced with lat, lon, alt,
+    and returns a pandas DataFrame with the data.
+    """
     return pd.read_csv(
         file_path,
         names=["timestamp", "lat", "lon", "alt", "qx", "qy", "qz", "qw"],
@@ -99,30 +100,47 @@ def read_tum_gt(file_path: str) -> pd.DataFrame:
 
 
 def write_tum_file(filename: str, data: np.ndarray):
+    """
+    Writes a TUM file from a numpy array.
+    """
     with open(filename, "w") as file:
         for row in data:
             file.write(" ".join(map(str, row)) + "\n")
 
 
 def write_tum_file_df(file_path: str, df: pd.DataFrame):
+    """
+    Writes a TUM file from a pandas DataFrame.
+    """
     df.to_csv(file_path, sep=" ", header=False, index=False)
 
 
 def read_map_points(file_path: str) -> pd.DataFrame:
+    """
+    Reads a Map Points file and returns a pandas DataFrame with the data.
+    """
     return pd.read_csv(file_path, names=["x", "y", "z"], delim_whitespace=True)
 
 
 def ensure_dir(dirpath: str):
+    """
+    Creates a directory if it does not exist.
+    """
     if not os.path.exists(dirpath):
         os.makedirs(dirpath, exist_ok=True)
 
 
 def to_xyz(tum_df: pd.DataFrame) -> np.ndarray:
+    """
+    Converts a TUM DataFrame to a numpy array of shape (n, 3) containing only the x, y, z columns.
+    """
     return tum_df[["x", "y", "z"]].to_numpy()
 
 
-# Converts TUM data to PoseTrajectory3D
 def create_pose_trajectory(data: np.ndarray) -> trajectory.PoseTrajectory3D:
+    """
+    Converts a numpy array of shape (n, 8) containing TUM data to a PoseTrajectory3D.
+    """
     stamps = data[:, 0]  # n x 1
     xyz = data[:, 1:4]  # n x 3
     quat = data[:, 4:]  # n x 4
@@ -134,13 +152,16 @@ def create_pose_trajectory(data: np.ndarray) -> trajectory.PoseTrajectory3D:
 def calculate_ape(
     trajectory: trajectory.PoseTrajectory3D, trajectory_gt: trajectory.PoseTrajectory3D
 ) -> metrics.APE:
+    """
+    Calculates the Absolute Pose Error (APE) between two trajectories.
+    """
     # Synchronize the two trajectories based on timestamps
     max_diff = 0.01  # Maximum timestamp difference for synchronization (e.g., 10 ms)
     synced_ground_truth_traj, synced_estimated_traj = sync.associate_trajectories(
         trajectory_gt, trajectory, max_diff
     )
 
-    # Calculate Absolute Trajectory Error (ATE)
+    # Calculate Absolute Pose Error (APE)
     ape_metric = metrics.APE(metrics.PoseRelation.translation_part)
     ape_metric.process_data((synced_ground_truth_traj, synced_estimated_traj))
 
